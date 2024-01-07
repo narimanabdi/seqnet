5bf6ab9d0f7e1ba46a29211897d1a6ee4f645d4d
naabdi <narimanabdi@gmail.com>
2024-01-07 13:38:52 +0330

    refactor: Rename inference.py -> test.py refactor: Modify test.py for using JSON config feat: Add test.json file

--- test.py
+++ test.py
@@ -15,22 +15,28 @@ from prettytable import PrettyTable
 from models.utils import count_params
 import argparse
 from models.distances import Cosine_Distance, Euclidean_Distance
+from keras.metrics import Recall, Precision
+import json
 
-parser = argparse.ArgumentParser('Nearest Neighbor Test')
-parser.add_argument('--data',type = str,default='gtsrb2tt100k',help='Test Data')
-parser.add_argument('--device',type = str,default='cpu',choices=['cpu','gpu','rpi'])
-parser.add_argument('--dist',default='l2',choices=['l2','cosine'])
-parser.add_argument(
-    '--backbone',required=True,help='Backbone Network',
-    choices=['densenet','mobilenet','resnet','densenetmini'])
-args = parser.parse_args() 
+#read config
+json_file = open('cfgs/test.json')
+config = json.load(json_file)
+encoder_file = 'model_files/best_encoders/' + config['backbone'] + '_' + config['test'] + '_encoder.h5'
+device = config['device']
+distance_metric = config['distance']
+batch_size = config['batch_size']
 
-if args.dist == 'l2':
+if distance_metric == 'euclidean':
     dist_fn = Euclidean_Distance()
-elif args.dist == 'cosine':
+elif distance_metric == 'cosine':
     dist_fn = Cosine_Distance()
 #metric tracker
 acc_tracker = Mean(name='Nearest_Neighbor_Accuracy')
+rec_tracker = Mean(name='Nearest_Neighbor_Recall')
+pre_tracker = Mean(name='Nearest_Neighbor_Precision')
+_recall = Recall()
+_precision = Precision()
+
 time_tracker = Mean(name='Time')
 @tf.function
 def nn(model,inp,ztemplates):
@@ -52,20 +58,29 @@ def run_test(encoder_h5,data,batch):
     Zs = loaded_encoder(Xs)
     del Xs
     print('\033[0;32mStart Nearest Neighbor Evaluation\033[0m')
-    pb = tf.keras.utils.Progbar(len(test_generator),verbose=1)
+    #pb = tf.keras.utils.Progbar(len(test_generator),verbose=1)
     tval = []
     fpsval = []
-    for d,y_test in test_generator:
+    ITER = len(test_generator)
+    for iteration,(d,y_test) in enumerate(test_generator):
+        preds = []
+        targets = []
         for i,x in enumerate(d[1]):
             s = time()
             p = nn(loaded_encoder,x,Zs)
             tval.append(time() - s)
             fpsval.append(1.0 / tval[-1])
+            #we use argmax because we use negative distance as our metric so the max(- distance) = min(distance)
             if np.argmax(p) == np.argmax(y_test[i]):
                 acc_tracker.update_state(1.0)
             else:
                 acc_tracker.update_state(0.0)
-        pb.add(1)
+            preds += [np.argmax(p)]
+            targets += [np.argmax(y_test[i])]
+
+        rec_tracker.update_state(_recall(preds,targets))
+        pre_tracker.update_state(_precision(preds,targets))
+        print(f'iteration {iteration+1}/{ITER}, accuracy:{acc_tracker.result()*100.0:.2f}, recall:{rec_tracker.result()*100.0:.2f}, precision:{pre_tracker.result()*100.0:.2f}')
     #print report in table
     tval = np.asarray(tval)
     tmean = tf.math.reduce_mean(tval)
@@ -77,6 +92,8 @@ def run_test(encoder_h5,data,batch):
     myTable = PrettyTable([" 1-NN Evaluation Report", ""])
     myTable.add_row(["Evaluation Data", data])
     myTable.add_row(["Top-1 Accuracy", f'{acc_tracker.result()*100.0:.2f}'])
+    myTable.add_row(["Top-1 Recall", f'{rec_tracker.result()*100.0:.2f}'])
+    myTable.add_row(["Top-1 Precision", f'{pre_tracker.result()*100.0:.2f}'])
     myTable.add_row(["Model Parameters", f'{count_params(loaded_encoder):.2f}M'])
     myTable.add_row(["Average Frame Rate", f'{fpsmean:.2f}FPS'])
     myTable.add_row(["Frame Rate std", f'{fpsstd:.2f}FPS'])
@@ -85,16 +102,14 @@ def run_test(encoder_h5,data,batch):
     print('\033[0;31m')
     print(myTable)
     print('\033[0m')
-
 if __name__ == '__main__':
-    #encoder_file = 'model_files/best_encoders/' + args.backbone + '_' + args.data + '_encoder.h5'
-    encoder_file = 'model_files/best_encoders/densenet_tt100kft_encoder.h5'
-    if args.device == 'cpu':
+
+    if device == 'cpu':
         with tf.device('/cpu:0'):
-            run_test(encoder_h5=encoder_file,data=args.data, batch=128)
-    elif args.device == 'gpu':
+            run_test(encoder_h5=encoder_file,data=config['test'], batch=config['batch_size'])
+    elif device == 'gpu':
         with tf.device('/GPU:0'):
-            run_test(encoder_h5=encoder_file,data=args.data, batch=256)
-    elif args.device == 'rpi':
-        run_test(encoder_h5=encoder_file,data=args.data, batch=16)
+            run_test(encoder_h5=encoder_file,data=config['test'], batch=config['batch_size'])
+    elif device == 'rpi':
+        run_test(encoder_h5=encoder_file,data=config['test'], batch=config['batch_size'])
     
